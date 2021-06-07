1810ceb4bd0f5b327538adef50332a0e406703db
Ramiro Baltazar Vazquez Sanchez <ramirovazquez95@gmail.com>
2021-06-07 01:36:23 -0500

    Modify message

--- mealdelivery/notifications/utils.py
+++ mealdelivery/notifications/utils.py
@@ -3,9 +3,9 @@ import requests
 import os
 import logging
 
-# curl -X POST -H 'Content-type: application/json' --data '{"text":"Hola {employee.name} {} "}' https://hooks.slack.com/services/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
-# curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World! jluis avila"}' https://hooks.slack.com/services/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
-# curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World! jluis avila"}' https://hooks.slack.com/services/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
+# curl -X POST -H 'Content-type: application/json' --data '{"text":"Hola"}' https://hooks.slack.com/services/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
+# curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello"}' https://hooks.slack.com/services/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
+# curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello"}' https://hooks.slack.com/services/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
 
 def post_to_slack(message, SLACK_WEBHOOK_URL):
     data = {"text": message}
@@ -19,8 +19,9 @@ def send_message(
 ):
     site = Site.objects.get_current()
 
-    msg = f"Hi: {menu_link.employee.name} \n"
-    msg = msg + f"Select your menu : http://{site.domain}/menus/{menu_link.url_uuid}/ \n"
-    msg = msg + f"Fecha: {menu_link.menu.date_menu} \n"
+    msg = f"Hello!   \n"
+    msg = msg +  f"I share with you today's menu :)   \n"
+    msg = msg + f"For select options, go to: http://{site.domain}/menus/{menu_link.url_uuid}/ \n"
+    msg = msg + f"Menu date: {menu_link.menu.date_menu} \n"
 
     post_to_slack(msg, menu_link.employee.slack_webhook_url)
